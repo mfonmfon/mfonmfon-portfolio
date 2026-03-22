58838fe69c2ccb30c361fbb0849830106bc904c3
mfonmfon <mfonm579@gmail.com>
2026-03-22 04:25:58 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-black">
       <Header />
       <main>
         <Hero />
